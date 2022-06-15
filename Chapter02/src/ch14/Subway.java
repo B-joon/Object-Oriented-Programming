2ba008edb6e454488bcea763bd1d4207f85ab3f4
B-joon <dosel45@gmail.com>
2022-06-15 15:22:12 +0900

    객체 간의 협력

---
+++ Chapter02/src/ch14/Subway.java
@@ -0,0 +1,22 @@
+package ch14;
+
+public class Subway {
+
+	int subwayNumber;
+	int passengerCount;
+	int money;
+	
+	public Subway(int subwayNumber) {
+		this.subwayNumber = subwayNumber;
+	}
+	
+	public void take(int money) {
+		this.money += money;
+		passengerCount++;
+	}
+	
+	public void showSubwayInfo() {
+		System.out.println(subwayNumber + "번의 승객수는 " + passengerCount + "명 이고, 수입은 " + money + "원 입니다.");
+	}
+	
+}
